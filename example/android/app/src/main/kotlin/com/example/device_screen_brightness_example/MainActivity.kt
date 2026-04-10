b5bb5188fce0724872a32642f6882630d51f7e63
arcas0803 <arcas0803@gmail.com>
2026-04-10 11:23:31 +0200

    Initial releas

---
+++ example/android/app/src/main/kotlin/com/example/device_screen_brightness_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.device_screen_brightness_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/device_screen_brightness.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../device_screen_brightness.podspec for more information.
+#include "../../src/device_screen_brightness.c"
